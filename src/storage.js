4c8e8943babf0728c226b2b95576c556429f99b6
Bibhu Prasad <bibhup.behera@nettantra.net>
2022-04-13 18:51:49 +0530

    data added

---
+++ src/storage.js
@@ -0,0 +1,11 @@
+const storage = [{
+    name:"user1",
+    email:"user1@gmail.com",
+    phone:9876543210,
+    age:21,
+    address:{
+        city:"BBSR",
+        state:"Odisha",
+        zipCode:751002
+    }
+}]
